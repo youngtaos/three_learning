686929c14a2e544f6ac73b595f654adc0ca2d148
鸡杂不下面 <9583671+shredded-taro@user.noreply.gitee.com>
2023-11-11 16:56:57 +0800

    feat: add control

--- learn1/main.js
+++ learn1/main.js
@@ -1,13 +1,12 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 6
 camera.position.y = 2
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+
 
 const geometry = new THREE.BoxGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({ color: 'red' });
@@ -21,11 +20,24 @@ scene.add(cube);
 const gridHelper = new THREE.GridHelper(10, 10)
 scene.add(gridHelper)
 
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//add track 
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.addEventListener('change', function () {
+    console.log("change")
+})
 // animate the cube
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    controls.update()
     renderer.render(scene, camera);
 }
 animate();
