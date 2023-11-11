f63251328991d7bce84c86c62cb1bbdfd63c2af9
鸡杂不下面 <9583671+shredded-taro@user.noreply.gitee.com>
2023-11-11 16:13:22 +0800

    feat: start threejs learning

---
+++ learn1/main.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 6
+camera.position.y = 2
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
+
+//create cube
+const cube = new THREE.Mesh(geometry, material);
+cube.position.set(0, 2, 0)
+scene.add(cube);
+
+//crate grid 
+const gridHelper = new THREE.GridHelper(10, 10)
+scene.add(gridHelper)
+
+// animate the cube
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
