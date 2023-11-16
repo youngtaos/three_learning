6a743e913db791c6854d5ea6c1a83dfb4e333a02
鸡杂不下面 <9583671+shredded-taro@user.noreply.gitee.com>
2023-11-16 15:10:52 +0800

    feat: start with react

---
+++ learn2/src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import "./App.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+function App() {
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1, //近平面
+      1000 //远平面
+    );
+    camera.position.z = 6;
+    camera.position.y = 2;
+
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+    //create cube
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(0, 2, 0);
+    scene.add(cube);
+
+    //crate grid
+    const gridHelper = new THREE.GridHelper(10, 10);
+    scene.add(gridHelper);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    //add track
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.addEventListener("change", function () {
+      console.log("change");
+    });
+
+    //添加世界坐标辅助器
+    const axeHelper = new THREE.AxesHelper(5);
+    scene.add(axeHelper);
+
+    // animate the cube
+    function animate() {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      controls.update();
+      renderer.render(scene, camera);
+    }
+    animate();
+  }, []);
+  return <></>;
+}
+
+export default App;
